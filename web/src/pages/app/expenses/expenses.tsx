08f5c555b5d89db8feb2e9102da00282b08d1506
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-04-26 00:20:33 -0300

    ✨ feat: Add pagination component for expenses table page

--- web/src/pages/app/expenses/expenses.tsx
+++ web/src/pages/app/expenses/expenses.tsx
@@ -1,5 +1,6 @@
 import { Helmet } from 'react-helmet-async';
 
+import { Pagination } from '@/components/app-pagination';
 import {
   Table,
   TableBody,
@@ -18,41 +19,42 @@ export function Expenses() {
 
       <div className="flex flex-col gap-4">
         <h1 className="text-3xl font-bold tracking-tight">Despesas</h1>
-      </div>
+        <div className="space-y-2.5">
+          <ExpenseTableFilters />
 
-      <div className="space-y-2.5">
-        <ExpenseTableFilters />
+          <div className="rounded-md border">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-16"></TableHead>
 
-        <div className="rounded-md border">
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead className="w-16"></TableHead>
+                  <TableHead className="w-10.5">ID</TableHead>
+                  <TableHead>Título</TableHead>
+                  <TableHead className="w-48">Categoria</TableHead>
 
-                <TableHead className="w-10.5">ID</TableHead>
-                <TableHead>Título</TableHead>
-                <TableHead className="w-48">Categoria</TableHead>
+                  <TableHead className="w-35">Preço</TableHead>
+                  {/* <TableHead className="w-4">Cartão</TableHead> */}
 
-                <TableHead className="w-35">Preço</TableHead>
-                {/* <TableHead className="w-4">Cartão</TableHead> */}
+                  <TableHead>Fornecedor</TableHead>
+                  {/* <TableHead>CNPJ</TableHead> */}
 
-                <TableHead>Fornecedor</TableHead>
-                {/* <TableHead>CNPJ</TableHead> */}
+                  {/* <TableHead>Estado/Município</TableHead> */}
+                  <TableHead className="w-45">Criado há</TableHead>
 
-                {/* <TableHead>Estado/Município</TableHead> */}
-                <TableHead className="w-45">Criado há</TableHead>
+                  <TableHead className="w-41"></TableHead>
+                  <TableHead className="w-33"></TableHead>
+                </TableRow>
+              </TableHeader>
 
-                <TableHead className="w-41"></TableHead>
-                <TableHead className="w-33"></TableHead>
-              </TableRow>
-            </TableHeader>
+              <TableBody>
+                {Array.from({ length: 10 }).map((_, i) => {
+                  return <ExpenseTableRow key={i} />;
+                })}
+              </TableBody>
+            </Table>
+          </div>
 
-            <TableBody>
-              {Array.from({ length: 10 }).map((_, i) => {
-                return <ExpenseTableRow key={i} />;
-              })}
-            </TableBody>
-          </Table>
+          <Pagination pageIndex={0} totalCount={105} perPage={10} />
         </div>
       </div>
     </>
