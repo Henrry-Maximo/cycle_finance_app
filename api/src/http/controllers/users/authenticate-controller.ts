d1c33a8729447adc04d2d8e0c413c46af1114142
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-05-02 21:26:07 -0300

    ✨ feat: Add authenticate controller

---
+++ api/src/http/controllers/users/authenticate-controller.ts
@@ -0,0 +1,35 @@
+import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
+import { AuthenticateUseCase } from "@/use-cases/authenticate";
+import { InvalidCredentialsError } from "@/use-cases/errors/invalid-credentials-error";
+import { FastifyReply, FastifyRequest } from "fastify";
+import z from "zod";
+
+export async function authenticate(req: FastifyRequest, reply: FastifyReply) {
+  const authenticateBodySchema = z.object({
+    email: z.email(),
+    password: z.string().min(6).max(22)
+  });
+
+  const { email, password } = authenticateBodySchema.parse(req.body);
+
+  try {
+    const usersRepository = new PrismaUsersRepository();
+    const authenticateUseCase = new AuthenticateUseCase(usersRepository);
+
+    await authenticateUseCase.execute(
+      {
+        email,
+        password
+      }
+    );
+  } catch (err) {
+    if (err instanceof InvalidCredentialsError) {
+      return reply.status(400).send({ message: err.message });
+    }
+
+    // return reply.status(500).send(); // TODO: fix me
+    throw err; // se não for um erro conhecido, joga pra camada cima
+  }
+
+  return reply.status(200).send();
+}
--- api/src/http/routes.ts
+++ api/src/http/routes.ts
@@ -6,9 +6,10 @@ import { registerExpenses } from "./controllers/expenses/register-controller";
 import { getUsers } from "./controllers/users/get-controller";
 import { getExpenses } from "./controllers/expenses/get-controller";
 import { getCategories } from "./controllers/categories/get-controller";
+import { authenticate } from "./controllers/users/authenticate-controller";
 
 export async function appRoutes(app: FastifyInstance) {
-  // app.post("/session", authenticante)
+  app.post("/sessions", authenticate)
 
   app.get("/users", getUsers);
   app.get("/expenses", getExpenses);
