f144ac0404f5c17fabad31380578dfe527930f1d
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-05-09 15:01:50 -0300

    🧪 test: Add should be able to get categories list

---
+++ api/src/use-cases/get-categories.spec.ts
@@ -0,0 +1,47 @@
+import { expect, describe, it, beforeEach } from 'vitest';
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository';
+import { hash } from 'bcryptjs';
+import { InMemoryCategoriesRepository } from '@/repositories/in-memory/in-memory-categories-repository';
+import { GetCategoriesUseCase } from './get-categories';
+
+let usersRepository: InMemoryUsersRepository;
+let categoriesRepository: InMemoryCategoriesRepository;
+let sut: GetCategoriesUseCase;
+
+describe('Get Expenses Use Case', () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository();
+    categoriesRepository = new InMemoryCategoriesRepository();
+    sut = new GetCategoriesUseCase(categoriesRepository);
+  });
+
+  it('should be able to get categories list', async () => {
+    const userCreated = await usersRepository.create({
+      name: "John Doe",
+      email: "johndoe@example.com",
+      password_hash: await hash("123456", 6),
+    });
+
+    await categoriesRepository.create({
+      title: "Alimentação",
+      description: "Categoria criada para fiscalizar as compras de alimentos",
+      user: {
+        connect: { id: userCreated.id }
+      }
+    });
+
+    await categoriesRepository.create({
+      title: "Entretenimento",
+      description: "Categoria criada para fiscalizar as compras em relação a entretenimento",
+      user: {
+        connect: { id: userCreated.id }
+      }
+    });
+
+    const { categories } = await sut.execute();
+    console.log(categories);
+
+    expect(categories).toHaveLength(2);
+  });
+
+});
