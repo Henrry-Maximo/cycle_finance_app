6cc516e34ada2b57f92dc6c9d8b5d6395de8bcca
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-05-13 18:29:55 -0300

    🧪 test: Add verify if same category already exists

--- api/src/use-cases/register-categories.spec.ts
+++ api/src/use-cases/register-categories.spec.ts
@@ -4,6 +4,7 @@ import { InMemoryCategoriesRepository } from "@/repositories/in-memory/in-memory
 import { hash } from "bcryptjs";
 import { RegisterCategoriesUseCase } from "./register-categories";
 import { ResourceNotFoundError } from "./errors/resource-not-found-error";
+import { CategoryAlreadyExistsError } from "./errors/category-already-exists-error";
 
 let usersRepository: InMemoryUsersRepository;
 let categoriesRepository: InMemoryCategoriesRepository;
@@ -14,10 +15,7 @@ describe("Register Categories Use Case", () => {
     usersRepository = new InMemoryUsersRepository();
     categoriesRepository = new InMemoryCategoriesRepository();
 
-    sut = new RegisterCategoriesUseCase(
-      usersRepository,
-      categoriesRepository,
-    );
+    sut = new RegisterCategoriesUseCase(usersRepository, categoriesRepository);
   });
 
   it("should be able to register categories", async () => {
@@ -29,8 +27,9 @@ describe("Register Categories Use Case", () => {
 
     const { category } = await sut.execute({
       title: "Alimentação",
-      description: "Categoria criada para ser usada com qualquer tipo de compra que envolva alimentos.",
-      user_id: userCreated.id
+      description:
+        "Categoria criada para ser usada com qualquer tipo de compra que envolva alimentos.",
+      user_id: userCreated.id,
     });
 
     expect(category.title).toEqual(expect.any(String));
@@ -38,10 +37,35 @@ describe("Register Categories Use Case", () => {
   });
 
   it("should not be able to register category if user not exists", async () => {
-    await expect(async () => sut.execute({
+    await expect(async () =>
+      sut.execute({
+        title: "Alimentação",
+        description:
+          "Categoria criada para ser usada com qualquer tipo de compra que envolva alimentos.",
+        user_id: "user-01",
+      }),
+    ).rejects.toBeInstanceOf(ResourceNotFoundError);
+  });
+
+  it("should not be able to register category if same already exists", async () => {
+    const userCreated = await usersRepository.create({
+      name: "John Doe",
+      email: "johndoe@example.com",
+      password_hash: await hash("123456", 6),
+    });
+
+    const { category } = await sut.execute({
       title: "Alimentação",
       description: "Categoria criada para ser usada com qualquer tipo de compra que envolva alimentos.",
-      user_id: "user-01"
-    })).rejects.toBeInstanceOf(ResourceNotFoundError);
+      user_id: userCreated.id,
+    });
+
+    await expect(async () =>
+      sut.execute({
+        title: category.title,
+        description: category.description,
+        user_id: category.user_id,
+      }),
+    ).rejects.toBeInstanceOf(CategoryAlreadyExistsError);
   });
 });
