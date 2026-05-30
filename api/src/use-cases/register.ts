a389c0bbf970d9c06e48e05a51646286fcfdfbc0
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-05-30 15:52:45 -0300

    🐍 fix: Add email formatting register user

--- api/src/use-cases/register.ts
+++ api/src/use-cases/register.ts
@@ -7,28 +7,36 @@ interface RegisterUseCaseRequest {
   username: string;
   email: string;
   password: string;
-};
+}
 
 interface RegisterUseCaseResponse {
   user: User;
 }
 
 export class RegisterUseCase {
-  constructor(private usersRepository: UsersRepository) { };
+  constructor(private usersRepository: UsersRepository) {}
 
-  async execute({ username, email, password }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
-
-    const userWithSameEmail = await this.usersRepository.findByEmail(email);
+  async execute({
+    username,
+    email,
+    password,
+  }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
+    const emailFormatting = email.trim().toLowerCase();
+    const userWithSameEmail =
+      await this.usersRepository.findByEmail(emailFormatting);
 
     if (userWithSameEmail) {
       throw new UserAlreadyExistsError();
-    };
+    }
 
     // const prismaUsersRepository = new PrismaUsersRepository();
 
-    const user = await this.usersRepository.create({ name: username, email, password_hash: await hash(password, 6) });
+    const user = await this.usersRepository.create({
+      name: username,
+      email: emailFormatting,
+      password_hash: await hash(password, 6),
+    });
 
     return { user };
-  };
-};
-
+  }
+}
