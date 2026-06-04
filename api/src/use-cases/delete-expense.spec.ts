c40705f911c11240467743318afbfc7b9b83e30e
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-06-04 15:09:27 -0300

    🧪 test: Add more tests units for delete expense from user

--- api/src/use-cases/delete-expense.spec.ts
+++ api/src/use-cases/delete-expense.spec.ts
@@ -4,6 +4,8 @@ import { InMemoryUsersRepository } from "@/repositories/in-memory/in-memory-user
 import { hash } from "bcryptjs";
 import { beforeEach, describe, expect, it } from "vitest";
 import { DeleteExpenseUseCase } from "./delete-expense";
+import { NotAuthorizedError } from "./errors/not-authorized-error";
+import { ResourceNotFoundError } from "./errors/resource-not-found-error";
 
 let expensesRepository: InMemoryExpensesRepository;
 let categoriesRepository: InMemoryCategoriesRepository;
@@ -21,7 +23,7 @@ describe("Delete Expense Use Case", () => {
     );
   });
 
-  it("should be able to delete expense", async () => {
+  it("should be able to delete an expense", async () => {
     const userCreated = await usersRepository.create({
       name: "John Doe",
       email: "johndoe@example.com",
@@ -62,5 +64,66 @@ describe("Delete Expense Use Case", () => {
     await expect(
       sut.execute({ id, userId: user_id })
     ).resolves.toBeNull();
+  });
+
+  it("should not be able to delete an expense if it is not from the same user that attempting", async () => {
+    const userCreated = await usersRepository.create({
+      name: "John Doe",
+      email: "johndoe@example.com",
+      password_hash: await hash("123456", 6),
+    });
+
+    const userOther = await usersRepository.create({
+      name: "Robert Doe",
+      email: "robertdoe@example.com",
+      password_hash: await hash("123456", 6),
+    });
+
+    const categoryCreated = await categoriesRepository.create({
+      title: "Alimentação",
+      description:
+        "Categoria criada para ser usada com qualquer tipo de compra que envolva alimentos.",
+      user: {
+        connect: {
+          id: userCreated.id,
+        },
+      },
+    });
+
+    const { id } = await expensesRepository.create({
+      title: "Pães",
+      enterprise: "Mercado Ceifa",
+      description: "Café da manhã",
+      cnpj: "123.242.324.23/24",
+      source: "Embu das Artes / São Paulo",
+      price: 9.52,
+      card_last_digits: "2343",
+      user: {
+        connect: {
+          id: userCreated.id
+        }
+      },
+      category: {
+        connect: {
+          id: categoryCreated.id
+        }
+      }
+    });
+
+    await expect(
+      sut.execute({ id, userId: userOther.id })
+    ).rejects.toBeInstanceOf(NotAuthorizedError);
+  });
+
+  it("should not be able to delete an expense that does not exist from user", async () => {
+    const userCreated = await usersRepository.create({
+      name: "John Doe",
+      email: "johndoe@example.com",
+      password_hash: await hash("123456", 6),
+    });
+
+    await expect(
+      sut.execute({ id: 'non-expense', userId: userCreated.id })
+    ).rejects.toBeInstanceOf(ResourceNotFoundError);
   });
 });
