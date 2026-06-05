ad998859f72b0226c16b1f23af88d9114cb2961a
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-06-04 22:48:57 -0300

    🐍 fix: Add adjust at import prisma generate types

--- api/src/lib/prisma.ts
+++ api/src/lib/prisma.ts
@@ -1,6 +1,6 @@
 import { env } from "@/env";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "generated/prisma/client";
+import { PrismaClient } from "@/generated/prisma/client";
 
 const adapter = new PrismaPg({
   connectionString: env.DATABASE_URL!
