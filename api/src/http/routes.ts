1454d17218114e228c08dd4e9310b75101f5d1f2
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-06-27 22:29:05 -0300

    ✨ feat: Add fastify typed instance for working with the zod type provider

--- api/src/http/routes.ts
+++ api/src/http/routes.ts
@@ -1,10 +1,9 @@
-import { FastifyInstance } from "fastify";
-
 import { categoriesRoutes } from "./controllers/categories/routes";
 import { expensesRoutes } from "./controllers/expenses/routes";
 import { usersRoutes } from "./controllers/users/routes";
+import { FastifyTypedInstance } from "@/types";
 
-export async function appRoutes(app: FastifyInstance) {
+export async function appRoutes(app: FastifyTypedInstance) {
   app.register(usersRoutes);
   app.register(expensesRoutes);
   app.register(categoriesRoutes);
