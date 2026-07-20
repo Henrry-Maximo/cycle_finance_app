d69608a5181a02dccad848b3be0eacca4718e5bd
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-07-20 20:25:24 -0300

    ✨ feat: Add fetch expenses grouped by date use case

---
+++ api/src/use-cases/fetch-expenses-grouped-by-date.ts
@@ -0,0 +1,73 @@
+import { ExpensesRepository } from "@/repositories/expenses-repository";
+import { UsersRepository } from "@/repositories/users-repository";
+import { ResourceNotFoundError } from "./errors/resource-not-found-error";
+
+interface FetchExpensesGroupedByDateUseCaseRequest {
+  userId: string;
+  from?: Date;
+  to?: Date;
+}
+
+interface Expense {
+  createAt: string;
+  value: number;
+}
+
+interface FetchExpensesGroupedByDateUseCaseResponse {
+  expenses: Expense[];
+}
+
+export class FetchExpensesGroupedByDateUseCase {
+  constructor(
+    private usersRepository: UsersRepository,
+    private expensesRepository: ExpensesRepository,
+  ) {}
+
+  async execute({
+    userId,
+    from,
+    to,
+  }: FetchExpensesGroupedByDateUseCaseRequest): Promise<FetchExpensesGroupedByDateUseCaseResponse> {
+    const user = await this.usersRepository.findById(userId);
+
+    if (!user) {
+      throw new ResourceNotFoundError();
+    }
+
+    const today = new Date();
+    const fromStartDate = from
+      ? from
+      : new Date(today.getFullYear(), today.getMonth(), 1);
+    const toEndDate = to
+      ? to
+      : new Date(today.getFullYear(), today.getMonth() + 1, 0);
+
+    const expensesList = await this.expensesRepository.findManyByUserIdInPeriod(
+      userId,
+      fromStartDate,
+      toEndDate,
+    );
+
+    const transformed = expensesList.reduce(
+      (acc, current) => {
+        const date = current.created_at.toISOString().split("T")[0]!;
+
+        if (acc[date]) {
+          acc[date] += current.price;
+        } else {
+          acc[date] = current.price;
+        }
+
+        return acc;
+      },
+      {} as Record<string, number>,
+    );
+
+    const expenses = Object.entries(transformed).map(([date, value]) => ({
+      createAt: date,
+      value,
+    }));
+
+    return { expenses };
+  }
+}
