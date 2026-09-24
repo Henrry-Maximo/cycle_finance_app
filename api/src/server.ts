77c491b501d74ecddd748bcafd1caae2d4a06b6b
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-09-24 20:21:37 -0300

    ✨ feat: add database and redis connection check on startup

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,11 +1,29 @@
 import { app } from "./app";
 import { env } from "./env";
+import { prisma } from "./lib/prisma";
+import { redisClient } from "./lib/redis";
 
-app
-  .listen({
-    host: "0.0.0.0", // habilita acesso externo ao servidor
-    port: env.PORT, // porta do servidor
-  })
+async function main() {
+  await prisma.$connect();
+  await prisma.$queryRaw`SELECT 1`.catch(() => {
+    throw new Error("Failed to connect to database");
+  });
+
+  await redisClient.ping().catch(() => {
+    throw new Error("Failed to connect to Redis");
+  });
+
+  await app.listen({
+    host: "0.0.0.0",
+    port: env.PORT,
+  });
+}
+
+main()
   .then(() => {
-    console.log("HTTP Server Running! 🚀");
+    console.log(`HTTP Server Running! 🚀`);
+  })
+  .catch((err) => {
+    console.error(err.message);
+    process.exit(1);
   });
